80774fca8d4bce12bf78f8016d2979bd9b54d9cc
filip <filip.milkovic1@gmail.com>
2020-05-21 00:51:09 +0200

    Riješen

---
+++ LV3/ZAD2/MatrixGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD2
+{
+    class MatrixGenerator
+    {
+        private static MatrixGenerator instance;
+        private Random generatorNum;
+        private double[][] matrix;
+
+        private MatrixGenerator()
+        {
+            this.generatorNum = new Random();
+        }
+
+        public static MatrixGenerator GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new MatrixGenerator();
+            }
+            return instance;
+        }
+
+        public double[][] SetupMatrix(int rowNumber, int colNumber)
+        {
+            double[][] row = new double[rowNumber][];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                double[] column = new double[colNumber];
+                for (int j = 0; j < colNumber; j++)
+                {
+                    column[j] = this.generatorNum.NextDouble();
+                }
+                row[i] = column;
+            }
+            matrix = row;      
+            return this.matrix;
+        }
+        
+
+        public void Print()
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write("{0}\t", matrix[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ LV3/ZAD2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatrixGenerator matrix = MatrixGenerator.GetInstance();
+
+            matrix.SetupMatrix(4,4);
+
+            matrix.Print();
+
+        }
+    }
+}
