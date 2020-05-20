d05a251fe1c264464d2ba36eb68717f4b61b8f6d
filip <filip.milkovic1@gmail.com>
2020-05-20 19:35:50 +0200

    Riješen

---
+++ LV3/ZAD1/Dataset.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD1
+{
+    class Dataset : Prototype
+    {
+        private List<List<string>> data; //list of lists of strings
+        public Dataset()
+        {
+            this.data = new List<List<string>>();
+        }
+        public Dataset(string filePath) : this()
+        {
+            this.LoadDataFromCSV(filePath);
+        }
+        public void LoadDataFromCSV(string filePath)
+        {
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    List<string> row = new List<string>();
+                    string[] items = line.Split(',');
+                    foreach (string item in items)
+                    {
+                        row.Add(item);
+                    }
+                    this.data.Add(row);
+                }
+            }
+        }
+        public IList<List<string>> GetData()
+        {
+            return
+           new System.Collections.ObjectModel.ReadOnlyCollection<List<string>>(data);
+        }
+        public void ClearData()
+        {
+            this.data.Clear();
+        }
+
+        public Prototype Clone()
+        {
+            Dataset deepCopy = new Dataset();
+
+            for(int row = 0; row < data.Count; row++)
+            {
+                deepCopy.data.Add(new List<string>());
+                for(int col = 0; col < data[0].Count; col++)
+                {
+                    deepCopy.data[row].Add(this.data[row][col]);
+                }
+            }
+            return (Prototype)deepCopy;             
+        }
+    }
+}
---
+++ LV3/ZAD1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dataset dataFile = new Dataset(@"E:\Projects\GitHub\RPPOON-LV3\LV3\ZAD1\csv1.csv");
+
+            Dataset clonedFile = (Dataset)dataFile.Clone();
+
+            foreach(List<string> line in clonedFile.GetData())
+            {
+                foreach(string data in line)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+            
+        }
+    }
+}
