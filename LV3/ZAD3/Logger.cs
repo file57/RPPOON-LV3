b204a879acfbdbe671e872bfdda9b6e4c9182431
filip <filip.milkovic1@gmail.com>
2020-05-21 19:36:15 +0200

    Dio riješen

---
+++ LV3/ZAD3/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD3
+{
+    class Logger
+    {
+        private static Logger instance;
+        private string filePath;
+
+        private Logger()
+        {
+            this.filePath = @"E:\file.txt";
+        }
+
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+            return instance;
+        }
+        public void Log(string message)
+        {
+            using (System.IO.StreamWriter writer =
+                    new System.IO.StreamWriter(this.filePath))
+            {
+                writer.WriteLine(message);
+            }
+        }
+        public void SetFilePath(string filePath)
+        {
+            this.filePath = filePath;
+            GetInstance();
+        }
+    }
+}
---
+++ LV3/ZAD3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger log = Logger.GetInstance();
+            log.Log("Da");
+
+            log.SetFilePath(@"E:\Projects");
+            log.Log("Ne");
+        }
+    }
+}
